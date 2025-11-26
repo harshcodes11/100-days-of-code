f7cebb0936c49fa111b64d23a6bcd001e9125a0e
harshcodes11 <xvicton2277@gmail.com>
2025-11-26 10:35:50 +0530

    Update day73.c

---
+++ day73.c
@@ -0,0 +1,19 @@
+
+//Q123: Read a text file and count the total number of characters, words, and lines. 
+//A word is defined as a sequence of non-space characters separated by spaces or newlines.
+
+/*
+Sample Test Cases:
+Input 1:
+File: sample.txt (Content: Hello world\nThis is C programming)
+Output 1:
+Characters: 31
+Words: 5
+Lines: 2
+
+*/
+#include<stdio.h>
+
+int main() {
+    
+}
